3cc23ad828295ffca8244672a125356d665565ea
encloinc <jelch2002@gmail.com>
2024-11-27 20:05:01 -0600

    fix

--- src/new_index/schema.rs
+++ src/new_index/schema.rs
@@ -3,6 +3,7 @@ use bitcoin::hashes::sha256d::Hash as Sha256dHash;
 use bitcoin::merkle_tree::MerkleBlock;
 use bitcoin::VarInt;
 use sha2::{Sha256, Digest};
+use hex::FromHex;
 use itertools::Itertools;
 use rayon::prelude::*;
 
